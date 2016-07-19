fa0634168d1eec57baeae4b76396e21ec226bf28
Deborah Hamel <deborahleehamel@gmail.com>
2016-07-18 19:26:25 -0600

    implement endpoints for merchant records add merchants folder to api vi add find controller for merchants add random controller for merchants tested in postman

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get "/find_all", to: "find#index"
+        get "/find", to: "find#show"
+        get "/random", to: "random#show"
+      end
+      resources :merchants, only: [:index, :show]
+
       namespace :invoices do
         get "/find_all", to: "find#index"
         get "/find", to: "find#show"
