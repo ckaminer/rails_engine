ed5df56d568837da11a611d294e7de880e69d900
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-19 19:56:47 -0600

    correct spelling mistake in customer model spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   fixtures :customers
-  context "it can retrive associated transactions" do
+  context "it can retrieve associated transactions" do
     it "returns an array of associated transactions" do
       customer = customers(:one)
       expect(customer.transactions.count).to eq 2
