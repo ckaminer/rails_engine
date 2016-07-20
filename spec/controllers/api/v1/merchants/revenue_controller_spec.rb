89a5969cb60df449b6431af5327f9ad261f77301
Deborah Hamel <deborahleehamel@gmail.com>
2016-07-20 16:51:15 -0600

    implement biz logic for revenue by date add revenue_by_date method to return total revenue by date for a merchant method uses single query in merchant model added created_at dates to invoices fixture added conditional for date to api/v1/ merchants revenue_controller

--- spec/controllers/api/v1/merchants/revenue_controller_spec.rb
+++ spec/controllers/api/v1/merchants/revenue_controller_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe Api::V1::Merchants::RevenueController do
         expect(search).to eq 32
       end
     end
+
+    describe "GET index" do
+      fixtures :merchants
+      fixtures :transactions
+      fixtures :invoices
+      fixtures :invoice_items
+        it "can get total revenue for a single merchant by date" do
+          merchant = merchants(:one)
+          date = "2012-05-27 14:53:59"
+          get :index, id: merchant.id, date: date
+
+          expect(response.status).to eq 200
+          search = merchant.revenue_by_date(date)
+
+          expect(search).to eq 42
+        end
+      end
+
+
 end
