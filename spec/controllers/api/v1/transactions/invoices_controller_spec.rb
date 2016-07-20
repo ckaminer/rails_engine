630db9a883791b6f38569539021883945d74aced
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-19 18:20:21 -0600

    transaction invoice relationship test passing adjusted for new fixtures all tests passing

--- spec/controllers/api/v1/transactions/invoices_controller_spec.rb
+++ spec/controllers/api/v1/transactions/invoices_controller_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe Api::V1::Transactions::InvoicesController do
   fixtures :transactions
   fixtures :invoices
+  fixtures :customers
+  fixtures :merchants
   describe "GET show" do
     it "can get an invoice for a given transaction" do
       transaction = transactions(:one)
@@ -14,8 +16,8 @@ RSpec.describe Api::V1::Transactions::InvoicesController do
       parsed_invoice = JSON.parse(response.body)
 
       expect(parsed_invoice["status"]).to eq invoice.status
-      expect(parsed_invoice["customer"]).to eq invoice.customer
-      expect(parsed_invoice["merchant"]).to eq invoice.merchant
+      expect(parsed_invoice["customer_id"]).to eq invoice.customer.id
+      expect(parsed_invoice["merchant_id"]).to eq invoice.merchant.id
     end
   end
 end
